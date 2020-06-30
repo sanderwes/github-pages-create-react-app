315387ac0a5af359d5a0c7a33bbaad3c606d8d12
sander <sander.wessel@live.nl>
2020-06-30 12:40:32 +0200

    Updates

---
+++ precache-manifest.b90c25004f95346a0a3d02bf5dbf46a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57b0e9cab882bc2565003ea0a24a191",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "09b8bc2f793689cc63aa",
+    "url": "/github-pages-create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a2149d96d8e3f58a973",
+    "url": "/github-pages-create-react-app/static/js/2.61df284a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-create-react-app/static/js/2.61df284a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b8bc2f793689cc63aa",
+    "url": "/github-pages-create-react-app/static/js/main.f3d1e7d6.chunk.js"
+  },
+  {
+    "revision": "5ccb9087e2fd929f112b",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.ec1c1770.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
